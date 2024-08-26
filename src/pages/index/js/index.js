deb2dfc3bc4d076004aaf3621e9e5b7109c2d745
Traniks <andert2017@gmil.com>
2024-08-26 17:31:21 +0300

    BREAKING CHANGE: initialization

---
+++ src/pages/index/js/index.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+	window.addEventListener("scroll", (e)  => {
+        document.body.style.cssText += `--scrollTop: ${window.scrollY}px`;
+    })
+    gsap.registerPlugin(ScrollSmoother, ScrollTrigger)
+    ScrollSmoother.create({
+		wrapper: '.wrapper',
+		content: '.content',
+	})
+})
